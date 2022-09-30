1d701831624a17d40511d392abc5ba6b8517e4bd
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-30 10:27:57 -0400

    Update main.cpp
    add example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+// Mohawk College - COMP-10184 
+// 
+// Pulse Width Modulation - Test Program 
+// 
+#include <Arduino.h> 
+ 
+void setup() { 
+  // configure the USB serial monitor 
+  Serial.begin(115200); 
+ 
+  // configure pin D4 as a digital output - this is the LED 
+  pinMode(D4, OUTPUT); 
+ 
+  // set default PWM range 
+  analogWriteRange(1023); 
+} 
+ 
+ 
+void loop() { 
+  // turn the LED ON 
+  digitalWrite(D4, LOW); 
+  // wait 1s 
+  delay(1000); 
+ 
+  // turn the LED OFF 
+  digitalWrite(D4, HIGH); 
+  // wait 1s 
+  delay(1000); 
 }
